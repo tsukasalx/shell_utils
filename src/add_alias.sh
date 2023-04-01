4d84c06e8f4b40c1335aac3fa9ef8c0b0eeaeac8
tlx <wshrzzz@gmail.com>
2023-04-01 21:04:43 +0900

    add add_alias.sh

--- src/add_alias.sh
+++ src/add_alias.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+function print_help {
+  echo Usage: $(basename $0) [-y][-h] alias_name command
+}
+
 default_yes=0
-while getopts ":y" opt; do
+while getopts ":yh" opt; do
   case ${opt} in
+    h )
+      print_help
+      exit 0
+      ;;
     y )
       default_yes=1
       ;;
     \? )
       echo "Invalid option: -$OPTARG" 1>&2
+      print_help
       exit 1
       ;;
   esac
@@ -16,15 +25,20 @@ shift $((OPTIND -1))
 
 alias_name=$1
 cmd=${*:2}
+if [[ -z $alias_name || -z $cmd ]]; then
+  print_help
+  exit 1
+fi
+
 existing_alias=$(grep -o "^alias $alias_name=.*" ~/.bashrc)
 
 if [[ -n $existing_alias ]]; then
   if [[ $default_yes -eq 0 ]]; then
     read -p "The alias '$alias_name' already exists. Do you want to replace it with '$cmd'? [Y/n] " replace_alias
     case ${replace_alias} in
-        [Yy] )
+      [Yy] )
         ;;
-        * )
+      * )
         exit 0
         ;;
     esac
